fe790e9cb9652f85918425da7804e8883a0c2d1c
丈槍由紀 <chikorita157@ateliershiori.moe>
2022-01-13 19:17:19 -0500

    Save selected detect on sourceview

--- KaniManabu/Window Controllers/DeckBrowser.m
+++ KaniManabu/Window Controllers/DeckBrowser.m
@@ -42,7 +42,14 @@
 - (void)windowDidLoad {
     [super windowDidLoad];
     
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    if ([[NSUserDefaults standardUserDefaults] valueForKey:@"selecteddeck"]){
+        NSNumber *selected = (NSNumber *)[[NSUserDefaults standardUserDefaults] valueForKey:@"selecteddeck"];
+        [_sourceList selectRowIndexes:[NSIndexSet indexSetWithIndex: selected.unsignedIntegerValue]byExtendingSelection:false];
+    }
+    else{
+         [_sourceList selectRowIndexes:[NSIndexSet indexSetWithIndex:1]byExtendingSelection:false];
+    }
+    
     [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(receiveNotification:) name:@"DeckAdded" object:nil];
     [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(receiveNotification:) name:@"DeckRemoved" object:nil];
     [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(receiveNotification:) name:@"ReviewEnded" object:nil];
@@ -189,6 +196,7 @@
 
 - (void)sourceListSelectionDidChange:(NSNotification *)notification
 {
+    [[NSUserDefaults standardUserDefaults] setValue:@(_sourceList.selectedRow) forKey:@"selectedmainview"];
     [self loadDeck];
 }
 
